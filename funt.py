c0aeb6388002441c7e41c58bd2f303bca3948c64
KBasyal <kamalbasyal987@gmail.com>
2023-09-10 11:18:52 +0545

    trying to learn functions

---
+++ funt.py
@@ -0,0 +1,14 @@
+#In this file i will be learning about the python function
+def my_function():
+    print("hello world!")
+
+my_function();
+
+#another example for function with parameter
+
+def your_function(yourname):
+    print(yourname + "Basyal")
+
+your_function("kamal")
+your_function("anita")
+your_function("kabita")
