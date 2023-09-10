35874d3d44995a1f345d5694096685bc99351524
KBasyal <kamalbasyal987@gmail.com>
2023-09-10 08:28:52 +0545

    learning for loop

---
+++ demom.py
@@ -0,0 +1,15 @@
+# I am trying to learn for loop in detail
+print('hello world')
+# Starting for loop from here
+
+fruits=["Apple","Banana","Orange","Pineapple"]
+for fruit in fruits:
+    print(fruit)
+
+
+# givng another eample of for loop
+
+Subjects= ["Maths","Science","Social","Account","English"]
+for subject in Subjects:
+    print(subject)
+
