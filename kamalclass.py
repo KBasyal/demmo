91086fa6b94c69f9f1a8587ba2d618c365639c8e
KBasyal <kamalbasyal987@gmail.com>
2023-09-15 14:26:12 +0545

    making another class for practice

--- kamalclass.py
+++ kamalclass.py
@@ -1,9 +1,28 @@
 #learning class
-
 class person():
     name = "kamal"
     occupation = "Software developer"
     net_worth = 2000
+    def info(self):
+        print(f"{self.name} is a {self.occupation}")
 
 b = person()
 print(b.name,b.occupation)
+b.info()
+
+
+#creating another class and object to practice more
+
+class nepal():
+    citizens="neplese"
+    national_animale="cow"
+    national_bird="Lhophophorus"
+    def citizen(self):
+        print(f"The nationality of people living in Nepal is {self.citizens}")
+
+nation= nepal()
+
+print(nation.national_animale)
+
+nation.citizen()
+
